fad04f99e422ad97247dcf322133c71364bdb5ef
Amos Wenger <amos@bearcove.eu>
2026-01-27 11:03:12 +0100

    fix(dibs-admin): render children in TooltipRoot

--- packages/dibs-admin/src/lib/ui/tooltip/TooltipRoot.svelte
+++ packages/dibs-admin/src/lib/ui/tooltip/TooltipRoot.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
-	import { Tooltip as TooltipPrimitive } from "bits-ui";
+    import { Tooltip as TooltipPrimitive } from "bits-ui";
+    import type { Snippet } from "svelte";
 
-	let {
-		open = $bindable(false),
-		...restProps
-	}: TooltipPrimitive.RootProps = $props();
+    interface Props {
+        open?: boolean;
+        children?: Snippet;
+    }
+
+    let { open = $bindable(false), children }: Props = $props();
 </script>
 
 <TooltipPrimitive.Provider>
-	<TooltipPrimitive.Root bind:open {...restProps} />
+    <TooltipPrimitive.Root bind:open>
+        {@render children?.()}
+    </TooltipPrimitive.Root>
 </TooltipPrimitive.Provider>
